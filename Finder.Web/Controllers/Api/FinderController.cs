3cfcdab95a5d982e10f28cab1a016837e3429cff
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-02-09 22:47:02 +0100

    Removed examples controller and added "FinderController"

---
+++ Finder.Web/Controllers/Api/FinderController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Finder.Web.Controllers.Api
+{
+    public class FinderController : ApiController
+    {
+        public string Get()
+            => "Test Movie!";
+    }
+}
