1b572c31c83d1b3bec3794f82c06fe8cc88bf4e0
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-02-09 20:25:56 +0100

    Added Finder.Core for the core business logic

---
+++ Finder.Core/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finder.Core
+{
+    public class DumbClass
+    {
+        public void Test()
+        {
+            //In the class the core business logic will be implemented
+            throw new NotImplementedException();
+        }
+    }
+}
