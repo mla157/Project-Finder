894f75712ba1795d71f0344f91af8ea6994eafde
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-02-11 21:14:19 +0100

    Added movie test classes

---
+++ Finder.Web/Controllers/Api/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Finder.Core.Models;
+
+namespace Finder.Web.Controllers.Api
+{
+    public class MovieController : ApiController
+    {
+        public Movie Get()
+        {
+            var testMovie = new Movie();
+            testMovie.titleName = "Sharknado 3";
+            testMovie.description = "Beste wo gibt!";
+            testMovie.releaseDate = "21.02.2017";
+
+            return testMovie;
+        }
+    }
+}
---
+++ Finder.Web/Scripts/Client/Controller/MovieController.js
@@ -0,0 +1,27 @@
+﻿var app = angular.module('FinderApp'); // -> _Layout.cshtml <html ng-app="TemplateApp">
+
+app.controller("MovieController", MovieController);
+
+MovieController.$inject = ["$scope", "$http", "UrlService"];
+
+function MovieController($scope, $http, UrlService)
+{
+    $scope.status = 'Please wait';
+
+    $scope.getExampleText = function()
+    {
+        $http.get(UrlService.forApi('Movie')).then(function(response)
+        {
+            if (!response.data)
+            {
+                alert('Something went wrong!');
+            }
+
+            $scope.testMovie = response.data;
+        {
+            alert('Error!');
+        });
+    };
+
+    $scope.getExampleText();
+}
