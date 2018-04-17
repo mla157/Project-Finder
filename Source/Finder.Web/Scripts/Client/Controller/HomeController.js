9d0332b5f1c4ab50297f3665ffc614ac5009d5ee
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-04-17 09:13:46 +0200

    Added HomeController and Home

---
+++ Source/Finder.Web/Scripts/Client/Controller/HomeController.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('FinderApp');
+
+app.controller("HomeController", HomeController);
+
+HomeController.$inject = ["$scope", "$http", "UrlService"];
+
+function HomeController($scope, $http, UrlService)
+{
+    var userData;
+
+    $.getJSON('https://json.geoiplookup.io/api?callback=?', function (data) {
+        userData = JSON.stringify(data, null, 2);
+    });
+}
