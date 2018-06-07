96b2333ffa4468fa97d5ff6201ac54bdced8f7ad
Max Leon Albrecht <m.albrecht@faz.de>
2018-06-07 13:42:46 +0200

    Added context to ApiController

--- Source/Finder.Web/Controllers/Api/MoviesController.cs
+++ Source/Finder.Web/Controllers/Api/MoviesController.cs
@@ -8,14 +8,50 @@ using Finder.Core.Models;
 
 namespace Finder.Web.Controllers.Api
 {
+    using Models;
+
     public class MoviesController : ApiController
     {
         //In this method the connection to DB will be initialized and the movie date will be fetched and returned to the Frontend
-        public HttpResponseMessage Get()
+        public HttpResponseMessage Get(string genre)
         {
-            var testMovie = new Movie("Sharknado 3", "Beste wo gibt", DateTime.Parse("21.02.2016"));
+            var returnMovie = new MovieApiModel();
 
-            return this.Request.CreateResponse(HttpStatusCode.OK, testMovie);
+            if (genre == "action")
+            {
+                returnMovie.movieTitle = "Test Action";
+                returnMovie.MovieDateTime = DateTime.MaxValue;
+                returnMovie.movieGenre = "Test Action";
+
+            }
+            if (genre == "horror")
+            {
+                returnMovie.movieTitle = "Test Horror";
+                returnMovie.MovieDateTime = DateTime.MaxValue;
+                returnMovie.movieGenre = "Horror";
+
+            }
+            if (genre == "documentation")
+            {
+                returnMovie.movieTitle = "Test documentation";
+                returnMovie.MovieDateTime = DateTime.MaxValue;
+                returnMovie.movieGenre = "documentation";
+
+            }
+            if (genre == "scifi")
+            {
+                returnMovie.movieTitle = "Test scifi";
+                returnMovie.MovieDateTime = DateTime.MaxValue;
+                returnMovie.movieGenre = "scifi";
+            }
+            if (genre == "humor")
+            {
+                returnMovie.movieTitle = "Test humor";
+                returnMovie.MovieDateTime  = DateTime.MaxValue;
+                returnMovie.movieGenre = "humor";
+            }
+
+            return this.Request.CreateResponse(HttpStatusCode.OK, returnMovie);
         }
     }
 }
---
+++ Source/Finder.Web/Models/MovieApiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Finder.Web.Models
+{
+    public class MovieApiModel
+    {
+        public string movieTitle;
+
+        public string movieGenre;
+
+        public DateTime MovieDateTime;
+    }
+}
