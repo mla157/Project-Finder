f45b2e08ce9a52e4ba476809c496c6e7d088d769
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-06-09 12:21:40 +0200

    Fixed namespace

--- Source/Finder.Core/Models/DatabaseConnection.cs
+++ Source/Finder.Core/Models/DatabaseConnection.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data;
-
-namespace MysqlConnector
+﻿namespace Finder.Core.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using MySql.Data.MySqlClient;
+
     /// <summary>
     /// Class to connect to a mysql database server
     /// </summary>
