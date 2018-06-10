1f03b8bdb9c047129108f9736b585dae15493866
Max Leon Albrecht <max.leon.albrecht@googlemail.com>
2018-06-10 11:16:03 +0200

    Added class and controller for authentication

---
+++ Source/Finder.Web/Controllers/Api/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Finder.Web.Controllers.Api
+{
+    public class AuthenticationController : ApiController
+    {
+        public HttpResponseMessage Get(string genre)
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        public HttpResponseMessage Post(string genre)
+        {
+            return this.Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Source/Finder.Web/Models/UserApiModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Finder.Web.Models
+{
+    using System.Security.Cryptography;
+
+    public class UserApiModel
+    {
+        public string userName;
+
+        public string firstName;
+
+        public string lastName;
+
+        public string eMail;
+
+        public string password;
+    }
+}
